6a3365e985b48b43587971fb7478f7752547ad3a
BracketJohn <njonalik@protonmail.com>
2022-12-18 18:59:42 +0100

    fix: remove typescript to resolve #4

--- nuxt-prisma/server/middleware/0.prisma.js
+++ nuxt-prisma/server/middleware/0.prisma.js
@@ -1,13 +1,7 @@
 import { PrismaClient } from '@prisma/client'
 import { eventHandler } from 'h3'
 
-let prisma: PrismaClient
-declare module 'h3' {
-  interface H3EventContext {
-    prisma: PrismaClient
-  }
-}
-
+let prisma
 export default eventHandler((event) => {
   if (!prisma) {
     prisma = new PrismaClient()
